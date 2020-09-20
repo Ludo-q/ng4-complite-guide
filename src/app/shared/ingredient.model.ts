182411402fdf84f361ed898c9b1b45023c78c50c
Ludo-q <l.qerraxhija@gmail.com>
2020-09-20 19:00:56 +0200

    Creating and outputting the shopping list.

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  //
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // A shortcut for what we have up.
+
+  constructor(public name: string, public amount: number) {
+  }
+}
