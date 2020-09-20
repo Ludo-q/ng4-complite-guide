ed6f0d87476e5545e1d51ea792c0fcd815122afb
Ludo-q <l.qerraxhija@gmail.com>
2020-09-20 17:33:40 +0200

    Display recipe detail.

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test recepi 2', 'This is simple a test',
+      // tslint:disable-next-line:max-line-length
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
+    new Recipe('Test recepi 3', 'This is simple a test',
+      // tslint:disable-next-line:max-line-length
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg')
+  ];
+
 
   constructor() { }
 
