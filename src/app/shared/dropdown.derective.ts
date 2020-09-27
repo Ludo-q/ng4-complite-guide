cf3b438ffa27967167b1d2d4335819cc8eb06860
Ludo-q <l.qerraxhija@gmail.com>
2020-09-27 21:20:33 +0200

    Build the dropdown directive

--- src/app/shared/dropdown.derective.ts
+++ src/app/shared/dropdown.derective.ts
@@ -1,26 +1,18 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective implements OnInit {
+export class DropdownDirective {
 
-  opened = false;
+  @HostBinding('class.open')
+  isOpen = false;
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+  @HostListener('document:click', ['$event'])
+  toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 
-  ngOnInit(): void {
-  }
-
-  @HostListener('click', ['$event.target'])
-  mouseClick() {
-    if (this.opened) {
-      this.renderer.removeClass(this.elRef.nativeElement, 'open');
-      this.opened = false;
-    } else {
-      this.renderer.addClass(this.elRef.nativeElement, 'open');
-      this.opened = true;
-    }
+  constructor(private elRef: ElementRef) {
   }
 }
