eb85b68a7af83caf2978a934776277bd02bbdadf
Ludo-q <l.qerraxhija@gmail.com>
2022-11-29 11:44:12 +0100

    Observable compared to other techniques,
    c. compared to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
 
 import {LoggingService} from './logging.service';
 import {AuthService} from './auth/auth.service';
+import {concat, Observable, of} from 'rxjs';
+import {filter, find, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,46 @@ import {AuthService} from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
 
+  obsA = of(1, 2, 3, 5, 7);
+
+  obsB = of('a', 'b', 'c');
+
+  arrA = [1, 2, 3, 5, 7];
+  arrB = ['a', 'b', 'c'];
+
+
   constructor(
     private authService: AuthService,
     private loggingService: LoggingService,
     @Inject(PLATFORM_ID) private platformId
   ) {
+
+    // console.log('Concat');
+    // const obs = concat(this.obsA, this.obsB);
+    // const arr = this.arrA.concat(this.arrB);
+
+    // console.log('Filter');
+    // const obs = this.obsA.pipe(filter(res => res > 3));
+    // const arr = this.arrA.filter(item => item > 3);
+
+    // console.log('Find');
+    //
+    // const obs = this.obsB.pipe(find(res => res === 'b'));
+    // const result = this.arrB.find(item => item === 'b');
+
+    console.log('For each');
+    const obs = this.obsA.pipe(tap(res => console.log(res)));
+
+    console.log('Observable');
+    // obs.subscribe(res => console.log(res));
+    obs.subscribe();
+
+    console.log('Array');
+    // console.log(result);
+    this.arrA.forEach(item => console.log(item));
+
   }
+
 
   ngOnInit() {
     this.authService.autoSignIn();
